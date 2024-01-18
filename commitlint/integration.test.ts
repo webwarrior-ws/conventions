a0a0e7f45c85859466d46ab0a5b8ead7544f22de
webwarrior <reg@webwarrior.ws>
2024-01-18 12:11:29 +0100

    WIP: fix ts compiler errors
    Fix errros produced by TypeScript compiler when executing
    `npx tsc`.

--- commitlint/integration.test.ts
+++ commitlint/integration.test.ts
@@ -1,6 +1,6 @@
 let cp = require("child_process");
 
-function runCommitLintOnMsg(inputMsg: string) {
+function runCommitLintOnMsg2(inputMsg: string) {
     return cp.spawnSync("npx", ["commitlint", "--verbose"], {
         input: inputMsg,
     });
@@ -9,18 +9,18 @@ function runCommitLintOnMsg(inputMsg: string) {
 test("body-leading-blank1", () => {
     let commitMsgWithoutEmptySecondLine =
         "foo: this is only a title" + "\n" + "Bar baz.";
-    let bodyLeadingBlank1 = runCommitLintOnMsg(commitMsgWithoutEmptySecondLine);
+    let bodyLeadingBlank1 = runCommitLintOnMsg2(commitMsgWithoutEmptySecondLine);
     expect(bodyLeadingBlank1.status).not.toBe(0);
 });
 
 test("subject-full-stop1", () => {
     let commitMsgWithEndingDotInTitle = "foo/bar: bla bla blah.";
-    let subjectFullStop1 = runCommitLintOnMsg(commitMsgWithEndingDotInTitle);
+    let subjectFullStop1 = runCommitLintOnMsg2(commitMsgWithEndingDotInTitle);
     expect(subjectFullStop1.status).not.toBe(0);
 });
 
 test("subject-full-stop2", () => {
     let commitMsgWithoutEndingDotInTitle = "foo/bar: bla bla blah";
-    let subjectFullStop2 = runCommitLintOnMsg(commitMsgWithoutEndingDotInTitle);
+    let subjectFullStop2 = runCommitLintOnMsg2(commitMsgWithoutEndingDotInTitle);
     expect(subjectFullStop2.status).toBe(0);
 });
